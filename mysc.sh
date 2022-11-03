ff85512ebc8638ba0c58e6f4a24dc446b7419694
Mariia <mashyyza@gmail.com>
2022-11-03 19:13:29 +0200

    add home work files

---
+++ mysc.sh
@@ -0,0 +1,6 @@
+mkdir you_can
+cd you_can
+mkdir new_life qa_2023 canada_2023
+cd canada_2023
+touch file_1.txt file_2.txt file_3.txt file_4.txt file_5.txt file_1.json file_2.json file_3.json file_4.json file_5.json
+mkdir folder_1 folder_2 folder_3
